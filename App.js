9b059ba3ce07f0b3e85c6c7bf53e0ff5a00a6034
ilamkar <illum_dae@yahoo.com>
2020-03-14 14:08:00 -0500

    sending request and response from node

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-const fs = require("fs"); //file system
+const http = require("http");
 
-const userName = "ILAM KAR";
+const server = http.createServer((req, res) => {
+  console.log("Incoming Request");
 
-fs.writeFile("user-data.txt", "Name:" + userName, err => {
-  if (err) {
-    console.log(err);
-    return;
+  console.log(req.method, req.url);
+
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+      const userName = body.split('=')[1];
+      res.end('<h1>'+userName+ '</h1>')
+    });
+    req.on("data", chunk => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method ="POST"><input type="text" name="username"/><button type="submit">Create User</button></form>'
+    );
   }
-  console.log("Wrote file");
 });
+server.listen(5000);
