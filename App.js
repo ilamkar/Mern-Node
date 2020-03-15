767a461e817fea120bc52e8e8fab448090d57be0
ilamkar <illum_dae@yahoo.com>
2020-03-14 19:07:00 -0500

    routing setup

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
 const express = require ('express');
 const bodyParser= require('body-parser');
 
+const placesRoutes = require('./routes/places-routes');
+
 const app = express();
 
-app.use (bodyParser.urlencoded({
-    extended:false
-}));
+app.use(placesRoutes);
 
-app.post('/user',(req,res,next)=>{
-    res.send('<h1> User :' +req.body.username + '</h1>');  
-})
-
-/*
-app.use((req,res,next)=>{
-   let body='';   req.on('end',()=>{
-    const userName = body.split('=')[1];
-    if(userName){
-        req.body={name:userName};
-    }
-    
-    next();
-   });
-   req.on('data', chunk=>{
-       body+=chunk;
-   });
-});
-*/
-
-app.get('/',(req,res,next)=>{
- 
-    res.send('<form action ="/user" method="POST"><input type="text" name="username"/><button type="submit">Create User</button></form>');
-});
 
 app.listen(5000);
