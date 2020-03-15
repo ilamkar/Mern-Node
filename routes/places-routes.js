c9f9fd4da6d0b06561185fb5770c901133a161f5
ilamkar <illum_dae@yahoo.com>
2020-03-14 20:16:35 -0500

    route for place id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,37 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("Get Request in places");
-  res.json({ message: "It works" });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {  // register route
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place}); // => { place } => { place: place }
 });
 
-module.exports=router;
+router.get('/user/:uid', (req, res, next) => {  // register route
+    const userId = req.params.uid; 
+    const place = DUMMY_PLACES.find(p => {
+      return p.creator === userId;
+    });
+    res.json({place}); 
+  });
+  
+
+
+module.exports = router;
