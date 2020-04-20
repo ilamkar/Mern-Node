5eeda0a84194877dfe20c3094252562bb1c3f1d2
ilamkar <illum_dae@yahoo.com>
2020-04-20 10:34:52 -0500

    creatinf Okace schema and model

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const placesRoutes = require('./routes/places-routes');
-const userRoutes = require('./routes/users-routes')
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const userRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', userRoutes); // => /api/places...
+app.use("/api/places", placesRoutes);
+app.use("/api/users", userRoutes); // => /api/places...
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -21,8 +22,18 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb+srv://<ilamkar>:<pass>@cluster0-50wle.mongodb.net/places?retryWrites=true&w=majority')
+  .then(()=>{
+    app.listen(5000);
+  })
+  .catch( err=>{
+    console.log(err)
+  }
+
+  );
+
